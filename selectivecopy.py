cc1e0fc1916cb6d3f59dbde70a61c61d2614d12e
ganeshtiwari <blinkganesh@gmail.com>
2018-01-17 13:20:29 +0545

    fixed typos

--- selectivecopy.py
+++ selectivecopy.py
@@ -8,14 +8,15 @@ import shutil
 def selective_copy(folder):
     dest_folder = os.path.abspath('.')
 
-    for foldername, subfolder, files in`
-        if not (file_name.endswith('.pdf') or file_name.endswith('.jpg')):
-            continue
-        else:
-            folder_abspath = os.path.abspath(foldername)
-            file_name = os.path.join(folder, file_name)
-            print('Moving %s, to %s' % (file_name, dest_folder))
-            shutil.move(file_name, folder)
+    for foldername, subfolder, files in os.walk(folder):
+        for file_name in files:
+            if not (file_name.endswith('.pdf') or file_name.endswith('.jpg')):
+                continue
+            else:
+                folder_abspath = os.path.abspath(foldername)
+                file_name = os.path.join(folder_abspath, file_name)
+                print('Moving %s, to %s' % (file_name, dest_folder))
+                shutil.move(file_name, folder)
 
 
 def main():
